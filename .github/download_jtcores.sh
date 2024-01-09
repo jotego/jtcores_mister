03bd74a680a6b3117256bd01b9815429268cf097
Jose Tejada <jose.tejada@jotego.es>
2024-01-09 19:31:22 +0100

    Deleted legacy unused code

--- .github/download_jtcores.sh
+++ .github/download_jtcores.sh
@@ -33,49 +33,6 @@ download_repository() {
     popd > /dev/null 2>&1
 }
 
-copy_file() {
-    local SOURCE="${1}"
-    local TARGET="${2}"
-
-    mkdir -p "${TARGET%/*}"
-    cp -r "${SOURCE}" "${TARGET}"
-}
-
-is_not_rbf_release() {
-    is_not_file_extension "${1}" "rbf"
-}
-
-is_not_file_extension() {
-    local INPUT_FILE="${1}"
-    local EXPECTED_EXTENSION="${2}"
-    if is_file_extension "${INPUT_FILE}" "${EXPECTED_EXTENSION}" ; then
-        return 1
-    fi
-    >&2 echo "${INPUT_FILE} is NOT a ${EXPECTED_EXTENSION^^} file."
-    return 0
-}
-
-is_file_extension() {
-    local INPUT_FILE="${1}"
-    local EXPECTED_EXTENSION="${2}"
-    local ACTUAL_EXTENSION="${INPUT_FILE#*.}"
-    if [[ "${INPUT_FILE}" == "" ]] || [[ "${ACTUAL_EXTENSION,,}" != "${EXPECTED_EXTENSION,,}" ]] ; then
-        return 1
-    fi
-    return 0
-}
-
-files_with_no_date() {
-    local FOLDER="${1}"
-    pushd "${FOLDER}" > /dev/null 2>&1
-    for file in *; do
-        if ! [[ "${file}" =~ ^.+_([0-9]{8})(\..+)?$ ]] ; then
-            echo "${file}"
-        fi
-    done
-    popd > /dev/null 2>&1
-}
-
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]] ; then
     download_jtcores "${1}"
 fi
